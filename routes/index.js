0494c93114b2b695eac386a35587f2924bc2b6b3
Ben Fischer <ben.fischer.810@gmail.com>
2022-04-14 19:24:37 -0500

    convert content to jade templates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.get('/modules/*', function(req, res, next) {
+  res.render(`.${req.path}`);
 });
 
 module.exports = router;
